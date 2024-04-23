a195ed781d18d2c066d0bd3e21786fe10162c2b0
sam <abluecar@gmail.com>
2024-04-22 22:19:13 -0400

    style: Add banner, footer, CSS animations

--- src/home.js
+++ src/home.js
@@ -1,5 +1,34 @@
 export function populateHome() {
   const contentDiv = document.querySelector("#content");
   contentDiv.innerHTML = "";
-  contentDiv.textContent = "Hello from home.js!";
+  const main = document.createElement("main");
+  main.id = "main";
+
+  const bannerDiv = document.createElement("div");
+  bannerDiv.id = "banner";
+  bannerDiv.textContent = "Tacos are ____";
+  main.appendChild(bannerDiv);
+
+  const footerDiv = document.createElement("div");
+  footerDiv.id = "footer";
+  main.appendChild(footerDiv);
+
+  const addressDiv = document.createElement("div");
+  addressDiv.id = "address";
+  addressDiv.innerHTML = `<p>Address</p><p>12300 Main St.</p><p>Anywhere, TX 98700</p>`;
+  footerDiv.appendChild(addressDiv);
+
+  const dottedLineDiv = document.createElement("div");
+  dottedLineDiv.id = "dotted-line";
+  footerDiv.appendChild(dottedLineDiv);
+
+  const hoursDiv = document.createElement("div");
+  hoursDiv.id = "hours";
+  hoursDiv.innerHTML = `<p>Working hours</p>
+    <p>Monday to Saturday 8:00 AM – 9:00 PM</p>
+    <p>Sunday 9:00 AM – 9:00 PM</p>
+    <p>It's Monday 8:48 PM — Closing in 12 minutes</p>`;
+  footerDiv.appendChild(hoursDiv);
+
+  contentDiv.appendChild(main);
 }
